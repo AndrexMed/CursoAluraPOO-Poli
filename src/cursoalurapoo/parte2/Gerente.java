79a5e83e78e27abee07ffe37f5539fc8987fbace
AndrexMed <giovanniandrex@gmail.com>
2023-03-24 18:34:03 -0500

    Primeras aplicacion de herencia

--- src/cursoalurapoo/parte2/Gerente.java
+++ src/cursoalurapoo/parte2/Gerente.java
@@ -4,36 +4,15 @@ package cursoalurapoo.parte2;
  *
  * @author giova
  */
-public class Gerente {
-    private String nombre;
-    private String documento;
-    private double salario;
+public class Gerente extends Funcionario { // Gerente extiende de Funcionario - Aplicando herencia
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDocumento() {
-        return documento;
-    }
-
-    public void setDocumento(String documento) {
-        this.documento = documento;
-    }
-
-    public double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(double salario) {
-        this.salario = salario;
+    private String clave;
+    
+    public void setClave(String clave){
+        this.clave = clave;
     }
     
-    public double getBonificacion(){
-        return this.salario;
+    public boolean iniciarSesion(String clave){
+        return clave == "Gio";
     }
 }
--- src/cursoalurapoo/parte2/TestGerente.java
+++ src/cursoalurapoo/parte2/TestGerente.java
@@ -6,11 +6,15 @@ package cursoalurapoo.parte2;
  */
 public class TestGerente {
     public static void main(String[] args) {
-        //Gerente gerente = new Gerente();
+        Gerente gerente = new Gerente();
         //gerente.setSalario(5000);
-        Funcionario gerente = new Funcionario();
+        //Funcionario gerente = new Funcionario();
         gerente.setSalario(10000);
         gerente.setTipo(1);
+        gerente.setNombre("Giovanni");
+        gerente.setClave("Gio");
         System.out.println(gerente.getBonificacion());
+        System.out.println(gerente.getNombre());
+        System.out.println(gerente.iniciarSesion("Gio"));
     }
 }
