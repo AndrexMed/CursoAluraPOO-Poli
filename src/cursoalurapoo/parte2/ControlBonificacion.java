e9a2c02794d87c622f8f81a40b343912c8ac4bfa
AndrexMed <giovanniandrex@gmail.com>
2023-04-04 17:36:48 -0500

    Aplicando sobrecarga y remplazando por extends

--- src/cursoalurapoo/parte2/ControlBonificacion.java
+++ src/cursoalurapoo/parte2/ControlBonificacion.java
@@ -12,10 +12,21 @@ public class ControlBonificacion {
         System.out.println("Calculo actual: "+this.suma);
         return this.suma;
     }
+    //El codigo compila ya que el parametro que le estamos pidiendo es de tipo Funcionario y las demas clases
+    //heredan todos los atributos y comportamientos de Funcionarios...por lo cual no necesita la sobrecarga
     
-    public double registrarSalario(Gerente gerente){
+    //Sobrecarga de metodos
+    /*public double registrarSalario(Gerente gerente){
         this.suma += gerente.getBonificacion();
         System.out.println("Calculo actual: "+this.suma);
         return this.suma;
     }
+    
+    //Sobrecarga de metodos
+    public double registrarSalario(Contador contador){
+        this.suma += contador.getBonificacion();
+        System.out.println("Calculo actual: "+this.suma);
+        return this.suma;
+    }*/
+    
 }
--- src/cursoalurapoo/parte2/TestControlBonificacion.java
+++ src/cursoalurapoo/parte2/TestControlBonificacion.java
@@ -12,9 +12,13 @@ public class TestControlBonificacion {
         Gerente Niky = new Gerente();
         Niky.setSalario(10000);
         
+        Contador Lester = new Contador();
+        Lester.setSalario(5000);
+        
         ControlBonificacion controlbonificacion = new ControlBonificacion();
         
         controlbonificacion.registrarSalario(Morgan);
         controlbonificacion.registrarSalario(Niky);
+        controlbonificacion.registrarSalario(Lester);
     }
 }
